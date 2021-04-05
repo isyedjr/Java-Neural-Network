7e378f6d82a9811041a6ba5dd82552e4fcecb006
Imaad Syed <30536128+isyedjr@users.noreply.github.com>
2021-04-05 17:21:47 -0400

    Update NN.java

--- NN.java
+++ NN.java
@@ -1,6 +1,13 @@
 import java.util.*;
 import java.lang.*;
 
+/*
+ *
+ * A( Summation(previous layer output * weight)) 
+ * A(v) = 1 / (1 + e^-t)
+ * 
+ */
+
 public class NN {
     //layer 1
     double[][] weights1to2;
@@ -21,18 +28,21 @@ public class NN {
         layer3 = new double[m3];
         layer4 = new double[m4];
         
+        // random weights
         for(int a = 0, a < i; i++) {
             for(int j = 0; j < m1; j++) {
                 weights1to2[a][j] = 10.0 * Math.random();
             }
         }
         
+        // random weights
         for(int a = 0, a < m1; i++) {
             for(int j = 0; j < m2; j++) {
                 weights1to2[a][j] = 10.0 * Math.random();
             }
         }
         
+        // random weights
         for(int a = 0, a < m2; i++) {
             for(int j = 0; j < f; j++) {
                 weights1to2[a][j] = 10.0 * Math.random();
